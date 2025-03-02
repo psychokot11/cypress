2580f2d4c17e2c63bf732f446dfebb9ffef40928
Zofia Stawarska <z.stawarska@gmail.com>
2025-03-02 22:11:52 +0100

    multi-page testing

---
+++ cypress/e2e/examples.cy.js
@@ -0,0 +1,21 @@
+describe('Various examples', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    })
+    it('multi-page testing', () => {
+        cy.getDataTest('nav-why-cypress').click();
+        cy.location("pathname").should("equal", "/");
+        cy.getDataTest('nav-overview').click();
+        cy.location("pathname").should("equal", "/overview");
+        cy.getDataTest('nav-fundamentals').click();
+        cy.location("pathname").should("equal", "/fundamentals");
+        cy.getDataTest('nav-forms').click();
+        cy.location("pathname").should("equal", "/forms");
+        cy.getDataTest('nav-component').click();
+        cy.location("pathname").should("equal", "/component");
+        cy.getDataTest('nav-best-practices').click();
+        cy.location("pathname").should("equal", "/best-practices");
+        cy.getDataTest('nav-examples').click();
+        cy.location("pathname").should("equal", "/examples");
+    })
+})
