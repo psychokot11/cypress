ecc584bf46f43f4288c040b834fad0f87fa192c1
Zofia Stawarska <z.stawarska@gmail.com>
2025-03-09 18:17:20 +0100

    intercept 1

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,6 +1,6 @@
 describe('Various examples', () => {
     beforeEach(() => {
-        cy.visit('/');
+        cy.visit('/examples');
     })
     it('multi-page testing', () => {
         cy.getDataTest('nav-why-cypress').click();
@@ -15,7 +15,14 @@ describe('Various examples', () => {
         cy.location("pathname").should("equal", "/component");
         cy.getDataTest('nav-best-practices').click();
         cy.location("pathname").should("equal", "/best-practices");
-        cy.getDataTest('nav-examples').click();
-        cy.location("pathname").should("equal", "/examples");
+        // cy.getDataTest('nav-examples').click();
+        // cy.location("pathname").should("equal", "/examples");
+    })
+
+    it.only('intercepts', () => {
+        cy.intercept('POST', 'http://localhost:3000/examples', {
+            body: {message: 'successfully intercepted request'}
+        })
+        cy.getDataTest('post-button').click();
     })
 })
